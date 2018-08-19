47c21e8731ee63c5625000885018ed63bf0b5950
Jongbin Won <Jongbin.won@quoine.com>
2018-08-19 23:05:16 +0700

    d

--- src/static/sw.js
+++ src/static/sw.js
@@ -1,18 +1,21 @@
 importScripts('/static/cache-polyfill.js');
 
 // example usage:
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('demo-cache').then(function(cache) {
-      return cache.put('/', new Response("From the cache!"));
-    })
-  );
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('Zapp').then(function(cache) {
+     return cache.addAll([
+       '/static/css/main.css',
+     ]);
+   })
+ );
 });
-
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || new Response("Nothing in the cache for this request");
-    })
-  );
+ console.log(event.request.url);
+
+ event.respondWith(
+   caches.match(event.request).then(function(response) {
+     return response || fetch(event.request);
+   })
+ );
 });
