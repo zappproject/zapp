c2d7b0cd9bc76070d32ae781acea34447952b73b
Jongbin Won <Jongbin.won@quoine.com>
2018-08-27 02:54:51 +0700

    f

--- src/app.py
+++ src/app.py
@@ -26,12 +26,12 @@ c = Bitcoin()
 def initialize_database():
     Database.initialize()
 
-'''@app.before_request
+@app.before_request
 def before_request():
     if request.url.startswith('http://'):
         url = request.url.replace('http://', 'https://', 1)
         code = 301
-        return redirect(url, code=code)'''
+        return redirect(url, code=code)
 
 @app.route('/loginpage')
 def login_template():
